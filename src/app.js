5899f7d511f2dd443f7970a5ab8d8f2e49a2bde3
Mortcord <ratynsky98@gmail.com>
2023-10-09 23:33:48 +0300

    Remove test expense data

--- src/app.js
+++ src/app.js
@@ -5,25 +5,9 @@ import './styles/styles.css';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-    console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({description: 'Water bill', amount: 4500, createdAt: -21000}));
-store.dispatch(addExpense({description: 'Gas bill', amount: 1000, createdAt: -21000}));
-store.dispatch(addExpense({description: 'Rent', amount: 100950, createdAt: -21000}));
-
 
 const jsx = (
     <Provider store={store}>
